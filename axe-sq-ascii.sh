31a5e76dbe42016d6bda35dfb3bdf03207b6d27c
-k <slowdive@me.com>
2018-01-27 04:49:52 -0500

    Add files via upload

---
+++ axe-sq-ascii.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+cat << "EOF"                                                                                                    
+                                          `..-://:-----.                                            
+                                     `/yhdmMMMMMMMMMMMMMNmhs+:.                                     
+                                  ./oNMMNmMMNMmNNmmNMMMMMMMMMMNNdyo-..`--:. ``                      
+                             .:+hNNMMMMmdho-..`-.````./omMdNMMMMMMMNho:```                          
+                         ``ohNNdNyoo-:.``               ..-+ohNMMMMMMMNmy/                          
+                         `+/...`.         :.       `  `` ` `-::sNMMMMMMMMMds.                       
+                                          .+       `.```  `    `.hMmNMMMMMMMh.                      
+                         `                 +     ` ````   ```   ``oshNMMMMMMMm:                     
+                         .         `      `+`   `.    ` ``  `     ` .+dMMMMMMMN+                    
+                   `                       s    `                    -.dMMMMMMMMo.`                 
+         `     `  -y. `:                   /` `/    `   `             `sMMMMMMMMMh-                 
+         .` :/hy./N+`-d+         `          ` :`       ``.            -NMMMMMMMMMMh                 
+            hMMMmMM++NM+`                   :`` .`             `      /mMMMMMMMMMMM+`  `    `  ``   
+            sMMMMMMNMMMMh`              ` .-: ` .``                ```..mMMMMMMMMMMN/`.`` ```````   
+           `oMMMMMMMMMMMd+-`            `-/`    `       `               yMMMMMMMMMMMm`              
+          -dMMMMMMMMMMMMd/.              /  `       `   `           `  :NMMMMMMMMMMMM:              
+        `omNNmdMMMMMMMMMm               `   .`      `      `:+oooo.  .`+NMMMMMMMMMMMMo              
+         ...-hNMMMMMMMMM+          `  `..                 /mNNMMMMNho` .:dMMMMMMMMMMMy              
+          `-omMddodNMMMm.`.-/ossyyhdhosy``              `yMMMMMMMMMMMNy+..+mMMMMMMMMMd              
+         -++/NM``/-:dy/+hmMMMMMMMMMMMMMN/       ``  ``./mMMMMMMMMMMMMMMMMdo:yMMMMMMMMd              
+        `    yNy.oNm:`yMMMMMMMMMMMMMMMMMMh  `-//:--.:hMMMMMMMMMMMMMMMMMMMMMMdMMMMMMMMd              
+             :MMN.Nd`hMMMMMMMMMMMMMMMMMMMM.ohso   o `NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh              
+              dMM:dd NMMMMsoMMMMMMMMMMMMMMsy+:N  `y  dmMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMh              
+            :+sMMNMs NMMMMyNMMMMMMMMMMMMMm/s  y` .h  mMMMdMMMMMMMMMMMMMMMMMMMMMMMMMMMy              
+         `  +myMMMMN`MMMMMMMMMMMMMMMMMMMsom: `o . d` mMMMMMMMMMMMMMMMMMMMMMMMMNhMMMMMo              
+            `mmNsMMM`hMMMMMMMMMMMMMMMMMNNo` -mm`. oshMMMMMMMMMMMMMMMMMMMMMMMMMo+MMMMM/              
+             :yssMMM+ /dMMMMMMMMMMMMMMMm-   oMh `..-:mMMMMMMMMMMMMMMMMMMMMMMMm`sMMMMM-              
+              hMNMMMN` .shMMmmmdddmmo::`  - :mydNNMMNyomMMm-/mMMMMMMMMMMMNddy.-NMMMMM`              
+              shsMmMMs `  ./.`````...   /o- `yMMMMMMMMh`yMMo ../sys/oho-`-`/+oNMMMMMMs`             
+              .dmMNhhm:`  `--```       hms  /MMMMMMMMMMy`hMM-  ```` ```-ohNNMMMmMMMMMMh             
+       ` `    `NMMNo``:s+/.`    ++/:   +so :NMMMMMMMMMMM+`hMs.y+o+:`  .  -hMMMMyhMMMMMM-            
+               oNMMm+      . . `o::`  ...:`NMMMMMMMMMMMMm `Md-+dMdyo``../yNMd+/ydMMMMMM:            
+               ::NMNyho:-```     ``   `   .NMMMMMMMMMMNMN  +NhsNN+``./smNMMMmddNMMMMMMM-            
+                 -mMMMMNNmmmds/-ohy+`      -sMMMmydmNmmh/``:sNMMh-:ymMMMMMMMMMMMMMMMMMN``````````   
+                  -mMMMMMMMMMMMNNMMMh       /MN/../`..```` s:yMMNNNMMMMMMMMMMMMMMMMMMM:             
+                   .dMMMMMMMMMMMMMMMm       `:+   -        m`:MMMMMMMMMMMMMMMMMMMMMMN+              
+                    `yNMMMMMMMMMMhhM+  ` `  `    `.    ```.m``MMMMMMMMMMMMMMMMMMMMNd:               
+                      ./shmNMMMMMmsM`    `  `    ` `    ``./``mMMMMMMMMMMMMNmdhs+:-`                
+                          `-/sdNMMNN+       ``      `     ```:MMMMMMMMMMmy/-.                       
+                               /MNMMNs+/:::-..`     ``  `:h+:NMMMMMMMMMo`                           
+                               `d/NNhyMs+dy::y/.-+o.`/oosNNyhNymMMMMMMd``  `````                    
+                                `-dd+ys `/. .:  `/-  -h` s+--h:/MMMMMMh                             
+                                   .sMs:yo.`+``` .`   y``:mm/NmhMMMMMMN  ``                         
+                                    -MMMMMmdMhysohs+++NmdNMMMMMMMMMMMMM` .```...``...``````` ` ` `  
+                                     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN`           `                
+                                      +mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy`                            
+                                        :smMMMMMMMMMMMMMMMMMMMMMMMMMNs                              
+                                           -ohNMMMMMMMMMMMMMMMMMMNdo-``..-````                      
+                                               -://+oshhyyhyso/-...``` ``.`
+EOF
